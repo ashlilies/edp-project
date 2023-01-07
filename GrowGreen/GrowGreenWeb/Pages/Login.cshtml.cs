e396ec0e63588107e548a4ab56ee42186b2c32cf
Ashlee Tan <ash@ashlee.one>
2023-01-07 12:57:21 +0800

    some css and font work

---
+++ GrowGreen/GrowGreenWeb/Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GrowGreenWeb.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
