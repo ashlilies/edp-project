c4e1b871fd67357b7d3c8787ac7721d38c5a63bc
Vionaerika <vionaerikakwok@gmail.com>
2023-02-07 01:47:55 +0800

    fixing errors

--- GrowGreen/GrowGreenWeb/Pages/Lecturer/Courses/Index.cshtml.cs
+++ GrowGreen/GrowGreenWeb/Pages/Lecturer/Courses/Index.cshtml.cs
@@ -5,14 +5,21 @@ using System.Threading.Tasks;
 using GrowGreenWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 
 namespace GrowGreenWeb.Pages.Lecturer.Courses
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
         public List<Course> OngoingCourses { get; set; } = new();
         public List<Course> PastCourses { get; set; } = new();
+        public IList<Course> CoursesList;
+        public SelectList Courses;
+        public string SearchQuery { get; set; }
+        
         private readonly GrowGreenContext _context;
 
         public IndexModel(GrowGreenContext context)
@@ -34,8 +41,26 @@ namespace GrowGreenWeb.Pages.Lecturer.Courses
 
             OngoingCourses = courses.Where(c => c.EndDate >= DateTime.Today).ToList();
             PastCourses = courses.Where(c => c.EndDate < DateTime.Today).ToList();
+            SearchQuery = courses.Where(c => c.Name.Contains());
 
             return Page();
+        }
+        
+        public async Task OnGetAsync(string searchQuery)
+        {
+            IQueryable<string> genreQuery = from c in _context.Courses
+                orderby c.Name
+                select c.Name;
+
+            var courses = from c in _context.Courses
+                select c;
+
+            if (!String.IsNullOrEmpty(searchQuery))
+            {
+                courses = courses.Where(c => c.Name.Contains(searchQuery));
+            }
+            
+            courses = await c.ToListAsync();
         }
     }
 }
