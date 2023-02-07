c32af7f376052a7eb24e8d624073792f79e9903d
Vionaerika <vionaerikakwok@gmail.com>
2023-02-07 08:26:07 +0800

    search

--- GrowGreen/GrowGreenWeb/Pages/Lecturer/Courses/Index.cshtml.cs
+++ GrowGreen/GrowGreenWeb/Pages/Lecturer/Courses/Index.cshtml.cs
@@ -13,13 +13,15 @@ namespace GrowGreenWeb.Pages.Lecturer.Courses
 {
     public class IndexModel : PageModel
     {
-        [BindProperty]
         public List<Course> OngoingCourses { get; set; } = new();
         public List<Course> PastCourses { get; set; } = new();
+        public List<Course>? SearchResults { get; set; }
         public IList<Course> CoursesList;
         public SelectList Courses;
-        public string SearchQuery { get; set; }
-        
+
+        [BindProperty]
+        public string SearchQuery {get;set;}
+
         private readonly GrowGreenContext _context;
 
         public IndexModel(GrowGreenContext context)
@@ -27,40 +29,30 @@ namespace GrowGreenWeb.Pages.Lecturer.Courses
             _context = context;
         }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet(string? SearchQuery = null)
         {
             // todo: change to support account system :"D
             int lecturerId = TemporaryConstants.LecturerId;
 
-            List<Course> courses = await _context.Courses
+            var courses = _context.Courses
                 .Include(c => c.Lectures).ThenInclude(l => l.Videos)
                 .Include(c => c.Learners)
                 .Include(c => c.Chats)
-                .Where(c => c.LecturerId == lecturerId)
-                .ToListAsync();
+                .Where(c => c.LecturerId == lecturerId);
 
-            OngoingCourses = courses.Where(c => c.EndDate >= DateTime.Today).ToList();
-            PastCourses = courses.Where(c => c.EndDate < DateTime.Today).ToList();
-            SearchQuery = courses.Where(c => c.Name.Contains());
+
+            Console.WriteLine("search query: " + SearchQuery);
+            // if there is a search query, we filter the results here :"D
+            if (SearchQuery is not null)
+                SearchResults = await courses.Where(c => c.Name.Contains(SearchQuery)).ToListAsync();
+            else
+            {
+                OngoingCourses = await courses.Where(c => c.EndDate >= DateTime.Today).ToListAsync();
+                PastCourses = await courses.Where(c => c.EndDate < DateTime.Today).ToListAsync();
+            }
 
             return Page();
         }
-        
-        public async Task OnGetAsync(string searchQuery)
-        {
-            IQueryable<string> genreQuery = from c in _context.Courses
-                orderby c.Name
-                select c.Name;
-
-            var courses = from c in _context.Courses
-                select c;
-
-            if (!String.IsNullOrEmpty(searchQuery))
-            {
-                courses = courses.Where(c => c.Name.Contains(searchQuery));
-            }
-            
-            courses = await c.ToListAsync();
-        }
+       
     }
 }
