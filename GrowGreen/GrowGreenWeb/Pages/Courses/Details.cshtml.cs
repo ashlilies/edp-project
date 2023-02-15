70803114a825bbac0804345c5d6f8eb903225256
Ashlee Tan <ash@ashlee.one>
2023-02-15 22:02:08 +0800

    clean up courses page

--- GrowGreen/GrowGreenWeb/Pages/Courses/Details.cshtml.cs
+++ GrowGreen/GrowGreenWeb/Pages/Courses/Details.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GrowGreenWeb.Models;
+using GrowGreenWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +14,12 @@ namespace GrowGreenWeb.Pages.Courses
     {
         public Course Course { get; set; } = null!;
         private readonly GrowGreenContext _context;
+        private readonly AccountService _accountService;
 
-        public DetailsModel(GrowGreenContext context)
+        public DetailsModel(GrowGreenContext context, AccountService accountService)
         {
             _context = context;
+            _accountService = accountService;
         }
 
         public IActionResult OnGet(int id)
@@ -31,6 +34,18 @@ namespace GrowGreenWeb.Pages.Courses
                 return NotFound();
 
             Course = course;
+
+            // redirect to viewer if already signed up
+            User? user = _accountService.GetCurrentUser(HttpContext, AccountType.Learner);
+            if (user is not null)
+            {
+                _context.Attach(user);
+                if (course.CourseSignups.Select(cs => cs.Learner).Contains(user))
+                {
+                    return RedirectToPage("Viewer/Index", new { id = course.Id });
+                }
+            }
+
             return Page();
         }
     }
